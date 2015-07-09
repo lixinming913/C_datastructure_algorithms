ffc96b6df55284f5499b5447288c6a63ba5fe101
lixinming913 <xinyu913@163.com>
2015-07-09 22:18:14 +0800

    modifiy tab to bland

--- binarysearchtree_to_linktable.c
+++ binarysearchtree_to_linktable.c
@@ -8,11 +8,12 @@
 
 
 struct 
-BinaryTreeNode{
-    int                         m_nValue;
-    BinaryTreeNode*      m_pLeft;
-    BinaryTreeNode*      m_pRight;
-};
+ BinaryTreeNode
+ {
+    int              m_nValue;
+    BinaryTreeNode  *m_pLeft;
+    BinaryTreeNode  *m_pRight;
+ };
 
 
 void 
